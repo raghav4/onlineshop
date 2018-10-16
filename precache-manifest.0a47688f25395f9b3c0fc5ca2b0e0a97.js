f560a4f988c79dfee366818d1c76d328806054d8
Raghav Sharma <raghavsyt@gmail.com>
2018-10-16 11:22:43 +0530

    added the files

---
+++ precache-manifest.0a47688f25395f9b3c0fc5ca2b0e0a97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f01042259245e07ab84",
+    "url": "/static/js/main.0f010422.chunk.js"
+  },
+  {
+    "revision": "de623d67d5a629fdebe2",
+    "url": "/static/js/1.de623d67.chunk.js"
+  },
+  {
+    "revision": "0f01042259245e07ab84",
+    "url": "/static/css/main.dccc84d7.chunk.css"
+  },
+  {
+    "revision": "de623d67d5a629fdebe2",
+    "url": "/static/css/1.2dabbb7f.chunk.css"
+  },
+  {
+    "revision": "a6a7655e61cd97bf5ec4bb1a235e0903",
+    "url": "/index.html"
+  }
+];
